e78069ea88ad956e985465b5ab254a2a32931331
Nik Nyby <nnyby@columbia.edu>
2018-04-20 11:44:43 -0400

    setup.py updates

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='django-global-context',
       version='0.1.1',
       description='Global context injector for Django',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Nik Nyby',
       author_email='nnyby@columbia.edu',
       url='https://github.com/ccnmtl/django-global-context',
+      install_requires=['Django'],
       packages=['globalcontext'],
 )
